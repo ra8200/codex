1347d8a370d828f18a1b1d995000c4f999a4d4e3
ra8200 <razi.alawamleh@gmail.com>
2023-03-29 02:57:42 -0400

    second commit

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetching data from the server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-d0uq.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
